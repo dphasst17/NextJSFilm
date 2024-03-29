3f38b5243f5972220e79687fc44e02f21a86b9fd
dphasst17 <phathuu12@gmail.com>
2024-03-29 10:14:04 +0700

    update: add view user and staff, add contact page

--- client/src/app/pages/admin/modal/viewUserStaff.jsx
+++ client/src/app/pages/admin/modal/viewUserStaff.jsx
@@ -1,6 +1,10 @@
 'use client'
-import { Modal, ModalContent, ModalHeader, ModalFooter, ModalBody, Button} from "@nextui-org/react"
+import { StateContext } from "@/app/context/stateContext"
+import { Modal, ModalContent, ModalHeader, ModalFooter, ModalBody, Button, Code} from "@nextui-org/react"
+import { use, useEffect } from "react"
 const ModalViewUser = ({props}) => {
+    const {manager} = use(StateContext)
+    useEffect(() => {manager !== null && console.log(manager)},[manager])
     return <Modal
         isOpen={props.isOpen}
         onOpenChange={props.onOpenChange}
@@ -10,8 +14,8 @@ const ModalViewUser = ({props}) => {
             {(onClose) => (
                 <>
                     <ModalHeader className="flex flex-col gap-1">VIEW USER & STAFF</ModalHeader>
-                    <ModalBody className="flex flex-wrap !flex-row justify-around">
-                        
+                    <ModalBody className="flex flex-wrap !flex-row justify-between">
+                        {manager?.user?.dataUser.map(u => <Code className={`w-[48%] flex justify-between items-center ${u.action === "active" ? "bg-green-500" : 'bg-zinc-800'} bg-opacity-80 text-white text-[10px]`}>#{u.idUser} - {u.role === 1 ? 'Staff' : 'User'} - {u.dateCreated.split("-").reverse().join("/")}</Code>)}
                     </ModalBody>
                     <ModalFooter>
                         <Button>Add</Button>
